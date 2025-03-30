6ad1e522c6a9abdfd252758c4badd47751b239f4
Osamahfm <osamahamadafm@gmail.com>
2025-03-30 14:26:44 +0200

    editing java

--- home.js
+++ home.js
@@ -20,3 +20,33 @@ document.addEventListener("DOMContentLoaded", function() {
 function navigateToSignUp(){
     window.location.href = "signup.html";
 }
+document.getElementById("courseBtn").addEventListener("click", function () {
+            let dropdown = this.parentElement;
+            dropdown.classList.toggle("show");
+        });
+
+        document.addEventListener("click", function (event) {
+            let dropdown = document.querySelector(".dropdown");
+            if (!dropdown.contains(event.target)) {
+                dropdown.classList.remove("show");
+            }
+        });
+
+        document.addEventListener("DOMContentLoaded", function () {
+            const elements = document.querySelectorAll(".c1, .c2, .c3, .css1, .css2, .css3, .net1, .net2, .net3");
+
+            function checkScroll() {
+                elements.forEach((element) => {
+                    const elementTop = element.getBoundingClientRect().top;
+                    const windowHeight = window.innerHeight;
+
+                    if (elementTop < windowHeight - 100) {
+                        element.classList.add("show");
+                    }
+                });
+            }
+
+            window.addEventListener("scroll", checkScroll);
+            checkScroll(); 
+
+        });
