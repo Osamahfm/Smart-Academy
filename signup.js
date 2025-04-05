5c5815d71ab4685ac40d57580851f5323d2fb75e
Osamahfm <osamahamadafm@gmail.com>
2025-04-05 20:37:38 +0200

    rename

--- signup.js
+++
@@ -1,8 +0,0 @@
-function navigateToSignUp(){
-    window.location.href = "signup.html";
-}
-document.getElementById("signupform").addEventListener("submit", function(event){
-    event.preventDefault();
-    alert("Sign Up Successful");
-    signUp();
-});
