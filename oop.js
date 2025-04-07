06cfaf0c34d90db87cb13a220234f3ebb9f3e7e7
mahmoudkhaled2005 <modykhaledelansary@gmail.com>
2025-04-07 18:34:47 +0200

    Update oop.js

--- oop.js
+++ oop.js
@@ -9,3 +9,14 @@ document.addEventListener("click", function (event) {
         dropdown.classList.remove("show");
     }
 });
+function enrollUser() {
+    // Check if user is signed in
+    const isSignedIn = localStorage.getItem('isSignedIn');
+
+    if (isSignedIn === 'true') {
+        alert('You have been enrolled!');
+    } else {
+        // Redirect to login page
+        window.location.href = 'SignLog.html';
+    }
+}
