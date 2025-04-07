ffbac9e2a9cf1d7eb67044e3fa9261ce238ea23c
omar atef abdelwahaab <oatef4172@gmail.com>
2025-04-07 21:54:17 +0200

    up

--- home.js
+++ home.js
@@ -1,22 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const modal = document.getElementById("loginModal");
-    const btn = document.getElementById("log");
-    const span = document.getElementsByClassName("close")[0];
-
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-});
 function navigateToSignUp(){
     window.location.href = "signup.html";
 }
@@ -53,3 +34,4 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("scroll", checkScroll);
     checkScroll(); 
 });
+
