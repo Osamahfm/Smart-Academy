a64c510840947062926cb9d0afb0ad2a85883ef8
omar atef abdelwahaab <oatef4172@gmail.com>
2025-04-08 16:15:49 +0200

    up

--- oop/oop.js
+++ oop/oop.js
@@ -1,14 +1,3 @@
-document.getElementById("courseBtn").addEventListener("click", function () {
-    let dropdown = this.parentElement;
-    dropdown.classList.toggle("show");
-});
-
-document.addEventListener("click", function (event) {
-    let dropdown = document.querySelector(".dropdown");
-    if (!dropdown.contains(event.target)) {
-        dropdown.classList.remove("show");
-    }
-});
 function enrollUser() {
     // Check if user is signed in
     const isSignedIn = localStorage.getItem('isSignedIn');
