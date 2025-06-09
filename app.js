be0e277ae49b19ac0b47b616f35c05065d82359b
omar atef abdelwahaab <oatef4172@gmail.com>
2025-06-09 19:42:11 +0300

    ejs done
    v

--- app.js
+++ app.js
@@ -23,6 +23,46 @@ app.get('/contactUs', (req, res) => {
     res.render('contactUs'); 
 })
 
+app.get('/signlog', (req, res) => {
+    res.render('signlog');
+})
+
+app.get('/backend', (req, res) => {
+    res.render('backend');
+})
+
+app.get('/frontend', (req, res) => {
+    res.render('frontend');
+})
+
+app.get('/mobile', (req, res) => {
+    res.render('mobile');
+})
+
+app.get('/problemsolving', (req, res) => {
+    res.render('problemsolving');
+})
+
+app.get('/oop', (req, res) => {
+    res.render('oop');
+})
+
+app.get('/datastruct', (req, res) => {
+    res.render('datastruct');
+})
+
+app.get('/introcyber', (req, res) => {
+    res.render('introcyber');
+})
+
+app.get('/cyberspec', (req, res) => {
+    res.render('cyberspec');
+})
+
+app.get('/cehacker', (req, res) => {
+    res.render('cehacker');
+})
+
 app.listen(5050, () => {
     console.log("Server is running on port 5050");
 })
