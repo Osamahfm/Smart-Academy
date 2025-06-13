32c69c69c7791baf5f36366bb3fd65f35e9aa210
Osamahfm <osamahamadafm@gmail.com>
2025-06-13 11:48:32 +0300

    .

--- middlewares/routeNotFound.js
+++
@@ -1,6 +0,0 @@
-// middlewares/routeNotFound.js
-const { NotFoundError } = require('../utils/AppError');
-
-module.exports = (req, res, next) => {
-  next(new NotFoundError(`Can't find ${req.originalUrl} on this server!`));
-};
