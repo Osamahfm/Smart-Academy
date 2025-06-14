acb9124edadd0f6a5fa5eb9f85c5b7152a255d69
khaledmohamed23 <khaledmohamedahmed123@gmail.com>
2025-06-14 03:59:09 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,12 @@
-// ./config/db.js
-
 const mongoose = require('mongoose');
 const uri = 'mongodb+srv://osamahamadafm:VUImvoVhUiJ8xtUT@smartacagemy.isvdh60.mongodb.net/Smart';
 
 const connectDB = async () => {
   try {
     await mongoose.connect(uri);
-    console.log('✅ MongoDB Connected to Atlas database');
+    console.log('MongoDB Connected to Atlas database');
   } catch (err) {
-    console.error('❌ MongoDB connection failed:', err.message);
+    console.error('MongoDB connection failed:', err.message);
   }
 };
 
