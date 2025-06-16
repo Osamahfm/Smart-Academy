85d643a0b7ca550d2a8381f5a9da1649e8e975c7
Mahmoud khaled <modykhaledelansary@gmail.com>
2025-06-16 18:37:24 +0300

    Update Course.js

--- models/Course.js
+++ models/Course.js
@@ -18,7 +18,7 @@ const courseSchema = new mongoose.Schema({
     category: {
         type: String,
         required: [true, 'Course category is required'],
-        enum: ['frontend', 'backend', 'mobile', 'problemsolving', 'oop', 'datastruct', 'introcyber', 'cyberspec', 'cehacker']
+        enum: [ 'mobile', 'problemsolving', 'oop', 'datastruct', 'introcyber', 'cyberspec', 'cehacker']
     },
     imageUrl: {
         type: String,
